9d80d7bee3520bd6363467260d08d5d8f2df4dbd
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-11-13 19:23:52 +0300

    dz_1 update 13.11.2022

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,16 +14,20 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
+export type UserType = {
+    avatar: string
+    name: string
+}
+
+export type LocalMessageType = {
+    text: string
+    time: string
+}
+
 export type MessageType = {
     id: number
-    user: {
-        avatar: string
-        name: string
-    }
-    message: {
-        text: string
-        time: string
-    }
+    user: UserType
+    message: LocalMessageType
 }
 
 // структуру объекта не менять
