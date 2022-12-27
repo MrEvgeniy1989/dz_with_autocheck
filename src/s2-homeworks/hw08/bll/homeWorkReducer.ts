ea9a37b9e114b8aee2a2ba7def91f5c0471940dc
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2022-12-27 15:29:49 +0300

    dz_8 task completed 27.12.2022 Без стилей исправил ошибку Сорт мутировал массив, сделал копию
    - в дз 5 раскомментируйте <HW8/> в файле Junior.tsx, в этой дз нужно сделать hw08
    
    * 1 - дописать типы и логику (сортировка по имени, фильтрация по совершеннолетию) homeWorkReducer, проверить тестом
    * 2 - дописать компоненту User
    * 3 - сделать стили в соответствии с дизайном

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,11 +7,9 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            if (action.payload === 'up') {
-                return state.sort((a,b) => a.name > b.name ? 1 : -1)
-            } else if (action.payload === 'down') {
-                return state.sort((a,b) => a.name < b.name ? 1 : -1) // need to fix
-            } else return state
+            return action.payload === 'up'
+                ? [...state].sort((a,b) => a.name > b.name ? 1 : -1)
+                : [...state].sort((a,b) => a.name < b.name ? 1 : -1) // need to fix
         }
         case 'check': {
             return state.filter(el => el.age > action.payload) // need to fix
