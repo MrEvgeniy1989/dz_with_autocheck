2d762d717ef67a03b2c95b9449812b9f9f514e78
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-03-05 22:43:53 +0300

    dz_12 task completed 05.03.2023
    - в дз 5 раскомментируйте <HW12/> в файле JuniorPlus.tsx, в этой дз нужно сделать hw12
    
    * 1 - в файле themeReducer.ts написать нужные типы вместо any, дописать редьюсер
    * 2 - получить themeId из редакса
    * 3 - дописать тип и логику функции change
    * 4 - передать пропсы в SuperSelect

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,27 @@
-const initState = {
+type ActionType = ReturnType<typeof changeThemeId>
+type StateType = {
+    themeId: number
+}
+
+const initState: StateType = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state: StateType = initState, action: ActionType): StateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID': {
+            const copy = {...state}
+            return {...copy, themeId: action.id}
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type changeThemeIdType = {
+    type: 'SET_THEME_ID',
+    id: number
+}
+
+export const changeThemeId = (id: any): changeThemeIdType => ({type: 'SET_THEME_ID', id} as const)// fix any
