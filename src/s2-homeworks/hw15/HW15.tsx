7a8502e48c2d104708d4142f73b904da863e4f8f
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2023-03-25 00:09:00 +0300

    dz_15 task completed 25.03.2023 без стилей
    - в дз 5 раскомментируйте <HW15/> в файле JuniorPlus.tsx, в этой дз нужно сделать hw15
    (если у вас нет - склонируйте проект из дз1 повторно параллельно и заберите из него нужную папку)
    
    * 1 - дописать SuperPagination
    * 2 - дописать SuperSort
    * 3 - проверить pureChange тестами
    * 3 - дописать sendQuery, onChangePagination, onChangeSort в HW15
    * 4 - сделать стили в соответствии с дизайном
    * 5 - добавить HW15 в HW5/pages/JuniorPlus

--- src/s2-homeworks/hw15/HW15.tsx
+++ src/s2-homeworks/hw15/HW15.tsx
@@ -16,122 +16,124 @@ import SuperSort from './common/c10-SuperSort/SuperSort'
 * */
 
 type TechType = {
-    id: number
-    tech: string
-    developer: string
+  id: number
+  tech: string
+  developer: string
 }
 
 type ParamsType = {
-    sort: string
-    page: number
-    count: number
+  sort: string
+  page: number
+  count: number
 }
 
 const getTechs = (params: ParamsType) => {
-    return axios
-        .get<{ techs: TechType[], totalCount: number }>(
-            'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test3',
-            {params}
-        )
-        .catch((e) => {
-            alert(e.response?.data?.errorText || e.message)
-        })
+  return axios
+    .get<{ techs: TechType[], totalCount: number }>(
+      'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test3',
+      {params}
+    )
+    .catch((e) => {
+      alert(e.response?.data?.errorText || e.message)
+    })
 }
 
 const HW15 = () => {
-    const [sort, setSort] = useState('')
-    const [page, setPage] = useState(1)
-    const [count, setCount] = useState(4)
-    const [idLoading, setLoading] = useState(false)
-    const [totalCount, setTotalCount] = useState(100)
-    const [searchParams, setSearchParams] = useSearchParams()
-    const [techs, setTechs] = useState<TechType[]>([])
+  const [sort, setSort] = useState('')
+  const [page, setPage] = useState(1)
+  const [count, setCount] = useState(4)
+  const [idLoading, setLoading] = useState(false)
+  const [totalCount, setTotalCount] = useState(100)
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [techs, setTechs] = useState<TechType[]>([])
 
-    const sendQuery = (params: any) => {
-        setLoading(true)
-        getTechs(params)
-            .then((res) => {
-                // делает студент
-
-                // сохранить пришедшие данные
-
-                //
-            })
-    }
-
-    const onChangePagination = (newPage: number, newCount: number) => {
+  const sendQuery = (params: any) => {
+    setLoading(true)
+    getTechs(params)
+      .then((res) => {
         // делает студент
+        // сохранить пришедшие данные
+        if (res) {
+          setTotalCount(res.data.totalCount)
+          setTechs(res.data.techs)
+          setLoading(false)
+        }
+      })
+  }
 
-        // setPage(
-        // setCount(
+  const onChangePagination = (newPage: number, newCount: number) => {
+    // делает студент
 
-        // sendQuery(
-        // setSearchParams(
+    setPage(newPage)
+    setCount(newCount)
 
-        //
-    }
+    sendQuery({page: newPage, count: newCount, sort})
+    setSearchParams({page: newPage.toString(), count: newCount.toString()})
 
-    const onChangeSort = (newSort: string) => {
-        // делает студент
+    //
+  }
 
-        // setSort(
-        // setPage(1) // при сортировке сбрасывать на 1 страницу
+  const onChangeSort = (newSort: string) => {
+    // делает студент
 
-        // sendQuery(
-        // setSearchParams(
+    setSort(newSort)
+    setPage(1) // при сортировке сбрасывать на 1 страницу
 
-        //
-    }
+    sendQuery({page: 1, count, sort: newSort})
+    setSearchParams({page: '1', count: count.toString(), sort: newSort})
 
-    useEffect(() => {
-        const params = Object.fromEntries(searchParams)
-        sendQuery({page: params.page, count: params.count})
-        setPage(+params.page || 1)
-        setCount(+params.count || 4)
-    }, [])
+    //
+  }
 
-    const mappedTechs = techs.map(t => (
-        <div key={t.id} className={s.row}>
-            <div id={'hw15-tech-' + t.id} className={s.tech}>
-                {t.tech}
-            </div>
+  useEffect(() => {
+    const params = Object.fromEntries(searchParams)
+    sendQuery({page: params.page, count: params.count})
+    setPage(+params.page || 1)
+    setCount(+params.count || 4)
+  }, [])
 
-            <div id={'hw15-developer-' + t.id} className={s.developer}>
-                {t.developer}
-            </div>
+  const mappedTechs = techs.map(t => (
+    <div key={t.id} className={s.row}>
+      <div id={'hw15-tech-' + t.id} className={s.tech}>
+        {t.tech}
+      </div>
+
+      <div id={'hw15-developer-' + t.id} className={s.developer}>
+        {t.developer}
+      </div>
+    </div>
+  ))
+
+  return (
+    <div id={'hw15'}>
+      <div className={s2.hwTitle}>Homework #15</div>
+
+      <div className={s2.hw}>
+        {idLoading && <div id={'hw15-loading'} className={s.loading}>Loading...</div>}
+
+        <SuperPagination
+          page={page}
+          itemsCountForPage={count}
+          totalCount={totalCount}
+          onChange={onChangePagination}
+        />
+
+        <div className={s.rowHeader}>
+          <div className={s.techHeader}>
+            tech
+            <SuperSort sort={sort} value={'tech'} onChange={onChangeSort}/>
+          </div>
+
+          <div className={s.developerHeader}>
+            developer
+            <SuperSort sort={sort} value={'developer'} onChange={onChangeSort}/>
+          </div>
         </div>
-    ))
 
-    return (
-        <div id={'hw15'}>
-            <div className={s2.hwTitle}>Homework #15</div>
-
-            <div className={s2.hw}>
-                {idLoading && <div id={'hw15-loading'} className={s.loading}>Loading...</div>}
-
-                <SuperPagination
-                    page={page}
-                    itemsCountForPage={count}
-                    totalCount={totalCount}
-                    onChange={onChangePagination}
-                />
-
-                <div className={s.rowHeader}>
-                    <div className={s.techHeader}>
-                        tech
-                        <SuperSort sort={sort} value={'tech'} onChange={onChangeSort}/>
-                    </div>
-
-                    <div className={s.developerHeader}>
-                        developer
-                        <SuperSort sort={sort} value={'developer'} onChange={onChangeSort}/>
-                    </div>
-                </div>
-
-                {mappedTechs}
-            </div>
-        </div>
-    )
+        {mappedTechs}
+      </div>
+    </div>
+  )
 }
 
 export default HW15
--- src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
+++ src/s2-homeworks/hw15/common/c10-SuperSort/SuperSort.tsx
@@ -1,54 +1,61 @@
 import React from 'react'
+import arrowDown from './img/down.svg'
+import arrowUp from './img/up.svg'
+import arrows from './img/none.svg'
 
 // добавить в проект иконки и импортировать
-const downIcon = '[\\/]'
-const upIcon = '[/\\]'
-const noneIcon = '[--]'
+const downIcon = arrowDown
+const upIcon = arrowUp
+const noneIcon = arrows
 
 export type SuperSortPropsType = {
-    id?: string
-    sort: string
-    value: string
-    onChange: (newSort: string) => void
+  id?: string
+  sort: string
+  value: string
+  onChange: (newSort: string) => void
 }
 
 export const pureChange = (sort: string, down: string, up: string) => {
-    // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
-    return up // исправить
+  // пишет студент, sort: (click) => down (click) => up (click) => '' (click) => down ...
+  return sort === ''
+    ? down
+    : sort === down
+      ? up
+      : sort === up
+        ? ''
+        : down // исправить
 }
 
 const SuperSort: React.FC<SuperSortPropsType> = (
-    {
-        sort, value, onChange, id = 'hw15',
-    }
+  {
+    sort, value, onChange, id = 'hw15',
+  }
 ) => {
-    const up = '0' + value
-    const down = '1' + value
+  const up = '0' + value
+  const down = '1' + value
 
-    const onChangeCallback = () => {
-        onChange(pureChange(sort, down, up))
-    }
+  const onChangeCallback = () => {
+    onChange(pureChange(sort, down, up))
+  }
 
-    const icon = sort === down
-        ? downIcon
-        : sort === up
-            ? upIcon
-            : noneIcon
+  const icon = sort === down
+    ? downIcon
+    : sort === up
+      ? upIcon
+      : noneIcon
 
-    return (
-        <span
-            id={id + '-sort-' + value}
-            onClick={onChangeCallback}
-        >
-            {/*сделать иконку*/}
-            {/*<img*/}
-            {/*    id={id + '-icon-' + sort}*/}
-            {/*    src={icon}*/}
-            {/*/>*/}
 
-            {icon} {/*а это убрать*/}
+  return (
+    <span
+      id={id + '-sort-' + value}
+      onClick={onChangeCallback}
+    >
+            <img
+              id={id + '-icon-' + sort}
+              src={icon}
+            />
         </span>
-    )
+  )
 }
 
 export default SuperSort
